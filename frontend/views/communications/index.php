86e3d9c925265e5136983128ebedf541d58e9590
Charles Maina <charles.maina@attain-es.com>
2024-01-27 11:10:30 +0300

    frontend view communications to users based on membershiptype

--- frontend/views/communications/index.php
+++ frontend/views/communications/index.php
@@ -6,6 +6,7 @@ use yii\helpers\Url;
 use yii\grid\ActionColumn;
 use yii\grid\GridView;
 use yii\widgets\Pjax;
+
 /** @var yii\web\View $this */
 /** @var yii\data\ActiveDataProvider $dataProvider */
 
@@ -13,39 +14,35 @@ $this->title = 'Communications';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="communications-index">
-<div class="container">
-<h1><?= Html::encode($this->title) ?></h1>
-
-<p>
-    <?= Html::a('Create Communications', ['create'], ['class' => 'btn btn-success']) ?>
-</p>
-
-<?php Pjax::begin(); ?>
-
-<?= GridView::widget([
-    'dataProvider' => $dataProvider,
-    'columns' => [
-        ['class' => 'yii\grid\SerialColumn'],
-        'title',
-        ['label' => 'Membership Type', 'value' => function ($model) {
-            return $model->membershipType->name;
-        }],
-        ['label' => 'Communication Type', 'value' => function ($model) {
-            return $model->communicationType->name;
-        }],
-        [
-            'class' => ActionColumn::className(),
-            'template' => '{view}', // Only show the view action
-            'urlCreator' => function ($action, Communications $model, $key, $index, $column) {
-                return Url::toRoute([$action, 'id' => $model->id]);
-            }
-        ],
-    ],
-]); ?>
+    <div class="container">
+        <h1><?= Html::encode($this->title) ?></h1>
 
 
-<?php Pjax::end(); ?>
+        <?php Pjax::begin(); ?>
 
-</div>  
+        <?= GridView::widget([
+            'dataProvider' => $dataProvider,
+            'columns' => [
+                ['class' => 'yii\grid\SerialColumn'],
+                'title',
+                ['label' => 'Membership Type', 'value' => function ($model) {
+                    return $model->membershipType->name;
+                }],
+                ['label' => 'Communication Type', 'value' => function ($model) {
+                    return $model->communicationType->name;
+                }],
+                [
+                    'class' => ActionColumn::className(),
+                    'template' => '{view}', // Only show the view action
+                    'urlCreator' => function ($action, Communications $model, $key, $index, $column) {
+                        return Url::toRoute([$action, 'id' => $model->id]);
+                    }
+                ],
+            ],
+        ]); ?>
+
+
+        <?php Pjax::end(); ?>
+
+    </div>
 </div>
-  
